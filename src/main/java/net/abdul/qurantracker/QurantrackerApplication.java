6931f8b5f8c2b9afbaec70d301d802a7f69c6fc5
Abdul Wali Dal <148618788+abdulwalidal@users.noreply.github.com>
2025-10-11 21:44:56 +0500

    Initial commit - Quran Tracker project

---
+++ src/main/java/net/abdul/qurantracker/QurantrackerApplication.java
@@ -0,0 +1,13 @@
+package net.abdul.qurantracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QurantrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QurantrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/abdul/qurantracker/QurantrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.abdul.qurantracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QurantrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
