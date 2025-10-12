d1fb458416b5f86f0db4865acd195ff0c5d7079b
Abdul Wali Dal <148618788+abdulwalidal@users.noreply.github.com>
2025-10-13 01:03:45 +0500

    Add User entity with Lombok annotations and MongoDB integration

--- qurantracker/src/main/java/com/abdul/qurantracker/Entity/User.java
+++ qurantracker/src/main/java/com/abdul/qurantracker/Entity/User.java
@@ -1,7 +1,20 @@
 package com.abdul.qurantracker.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
+@Data
+@NoArgsConstructor
 public class User {
+
+    @Id
+    private ObjectId id;
+    private String userName;
+    private String password;
+
+
 
 }
