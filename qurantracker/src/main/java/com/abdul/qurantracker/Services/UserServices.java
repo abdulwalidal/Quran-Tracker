c9139bbc8062e5609dc2dcb029976b93b9e78ecf
Abdul Wali Dal <148618788+abdulwalidal@users.noreply.github.com>
2025-10-13 01:04:04 +0500

    Add UserServices to handle user creation and retrieval

--- qurantracker/src/main/java/com/abdul/qurantracker/Services/UserServices.java
+++ qurantracker/src/main/java/com/abdul/qurantracker/Services/UserServices.java
@@ -1,4 +1,28 @@
 package com.abdul.qurantracker.Services;
 
+
+import com.abdul.qurantracker.Entity.User;
+import com.abdul.qurantracker.Entity.VerseEntry;
+import com.abdul.qurantracker.Repository.UserRepository;
+import com.abdul.qurantracker.Repository.VerseEntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class UserServices {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public void createUser(User user) {
+        userRepository.save(user);
+    }
+
+    public List<User> getAll () {
+        return userRepository.findAll();
+    }
+
+
 }
