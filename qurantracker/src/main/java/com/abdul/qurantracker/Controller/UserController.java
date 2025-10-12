8c3e75a6cb526d618916bea46905392ae30dff41
Abdul Wali Dal <148618788+abdulwalidal@users.noreply.github.com>
2025-10-13 01:03:55 +0500

    Implement UserController with endpoints for creating and retrieving users

--- qurantracker/src/main/java/com/abdul/qurantracker/Controller/UserController.java
+++ qurantracker/src/main/java/com/abdul/qurantracker/Controller/UserController.java
@@ -1,6 +1,29 @@
 package com.abdul.qurantracker.Controller;
 
+import com.abdul.qurantracker.Entity.User;
+import com.abdul.qurantracker.Services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
+@RestController
+@RequestMapping("user")
 public class UserController {
+
+    @Autowired
+    UserServices userServices;
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+       userServices.createUser(user);
+       return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public List<User> getAll() {
+        return userServices.getAll();
+    }
 }
