e70c1b65a847f269e6b0a7ae9805da7dd96e3765
Abdul Wali Dal <148618788+abdulwalidal@users.noreply.github.com>
2025-10-13 01:04:23 +0500

    Implement VerseEntryController with CRUD operations for verse entries

--- qurantracker/src/main/java/com/abdul/qurantracker/Controller/VerseEntryController.java
+++ qurantracker/src/main/java/com/abdul/qurantracker/Controller/VerseEntryController.java
@@ -1,13 +1,44 @@
 package com.abdul.qurantracker.Controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.abdul.qurantracker.Entity.VerseEntry;
+import com.abdul.qurantracker.Services.UserServices;
+import com.abdul.qurantracker.Services.VerseEntryServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/verse")
 public class VerseEntryController {
+    @Autowired
+    VerseEntryServices verseEntryServices;
 
+    @PostMapping
+    public Boolean saveEntry(@RequestBody VerseEntry myEntry) {
+        verseEntryServices.saveEntry(myEntry);
+        return true;
+    }
+
+    @GetMapping
+    public List<VerseEntry> getAll() {
+       return verseEntryServices.getALl();
+
+    }
+
+    @DeleteMapping
+    public Boolean deleteEntry() {
+        verseEntryServices.deleteEntry();
+        return true;
+    }
+
+
+//
+//    public List<VerseEntry> updateEntryby() {
+//
+//    }
 
 }
 
