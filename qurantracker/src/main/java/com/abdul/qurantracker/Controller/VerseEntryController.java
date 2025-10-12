b59115bed229462a8ef0ac587e67c8d135c58d30
Abdul Wali Dal <148618788+abdulwalidal@users.noreply.github.com>
2025-10-12 23:50:58 +0500

    Implemented VerseEntryController with REST endpoints for managing verse entries through the QuranTracker API.

---
+++ qurantracker/src/main/java/com/abdul/qurantracker/Controller/VerseEntryController.java
@@ -0,0 +1,13 @@
+package com.abdul.qurantracker.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/verse")
+public class VerseEntryController {
+
+
+}
+
