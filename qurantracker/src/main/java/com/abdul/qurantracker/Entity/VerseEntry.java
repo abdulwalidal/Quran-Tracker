21eb68273ccae0f10342b0da04037a388340d1a0
Abdul Wali Dal <148618788+abdulwalidal@users.noreply.github.com>
2025-10-13 01:04:14 +0500

    Refactor VerseEntry entity to use Lombok annotations and standardize field naming

--- qurantracker/src/main/java/com/abdul/qurantracker/Entity/VerseEntry.java
+++ qurantracker/src/main/java/com/abdul/qurantracker/Entity/VerseEntry.java
@@ -1,16 +1,23 @@
 package com.abdul.qurantracker.Entity;
 
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+
 @Document(collection = "verse_entries")
+@Data
+@NoArgsConstructor
 public class VerseEntry {
+    @Id
+    private ObjectId id;
 
-    private String Surah;
+    private String surah;
     private String verseNo;
-    private boolean Memorized;
-
-
+    private boolean memorized;
 }
-
-
