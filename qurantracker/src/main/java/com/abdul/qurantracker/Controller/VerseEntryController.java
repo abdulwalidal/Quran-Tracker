07a09b22a74627759f1f7e999a0a9321fad2e6a0
Abdul Wali Dal <148618788+abdulwalidal@users.noreply.github.com>
2025-10-24 22:00:19 +0500

    Enhance VerseEntryController and VerseEntryServices: update saveEntry method to validate user existence and return ResponseEntity

--- qurantracker/src/main/java/com/abdul/qurantracker/Controller/VerseEntryController.java
+++ qurantracker/src/main/java/com/abdul/qurantracker/Controller/VerseEntryController.java
@@ -5,6 +5,8 @@ import com.abdul.qurantracker.Entity.VerseEntry;
 import com.abdul.qurantracker.Services.UserServices;
 import com.abdul.qurantracker.Services.VerseEntryServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -16,11 +18,15 @@ public class VerseEntryController {
     @Autowired
     VerseEntryServices verseEntryServices;
 
-    @PostMapping
-    public Boolean saveEntry(@RequestBody VerseEntry myEntry) {
-        verseEntryServices.saveEntry(myEntry);
-        return true;
-    }
+    @PostMapping("/{userName}")
+    public ResponseEntity<?> saveEntry(@RequestBody VerseEntry myEntry, @PathVariable String userName ) {
+         try { verseEntryServices.saveEntry(myEntry, userName);
+             return ResponseEntity.ok("Verse added Successfully"); }
+         catch(Exception e) {
+             return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error" + e.getMessage());
+        }
+        }
+
 
     @GetMapping
     public List<VerseEntry> getAll() {
--- qurantracker/src/main/java/com/abdul/qurantracker/Services/VerseEntryServices.java
+++ qurantracker/src/main/java/com/abdul/qurantracker/Services/VerseEntryServices.java
@@ -1,5 +1,6 @@
 package com.abdul.qurantracker.Services;
 
+import com.abdul.qurantracker.Entity.User;
 import com.abdul.qurantracker.Entity.VerseEntry;
 import com.abdul.qurantracker.Repository.VerseEntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +15,29 @@ public class VerseEntryServices {
 
     @Autowired
     VerseEntryRepository verseEntryRepository;
+    @Autowired
+    UserServices userService;
 
-    public void saveEntry(VerseEntry myEntry) {
-        verseEntryRepository.save(myEntry);
+
+    // Before saving any entry, the user must exist — so we need to find that user first.
+    public void saveEntry(VerseEntry myEntry, String userName) {
+        User user = userService.getUserbyUsername(userName);
+        if(user==null) {
+            throw new RuntimeException("User not found" + userName);
+        }
+        VerseEntry verseEntry = verseEntryRepository.save(myEntry);
+        user.getVerseEntry().add(verseEntry);
+        userService.createUser(user);
     }
+
 
     public List<VerseEntry> getALl () {
         return verseEntryRepository.findAll();
     }
 
-
     public void deleteEntry() {
         verseEntryRepository.deleteAll();
     }
+
+
 }
